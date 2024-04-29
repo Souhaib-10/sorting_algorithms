ec2038ee198c9d4a8831d6dd1984828b88a3d3e0
souhaib10 <souhaibb1998@gmail.com>
2024-04-29 06:50:17 -0700

    create bubble sort

--- sorting_algorithms/sort.h
+++ sorting_algorithms/sort.h
@@ -8,11 +8,15 @@
 #define UP 0
 #define DOWN 1
 
-/*enum bool - Enumeration of boolean values*/
+/*
+ * enum bool - Enumeration of boolean values
+ * false: Represents false with integer value 0
+ * true: Represents true with integer value 1
+ */
 typedef enum bool
 {
-    false = 0,
-    true
+	false = 0,  /**< Represents false with integer value 0 */
+	true       /**< Represents true with integer value 1 */
 } bool;
 
 
@@ -24,10 +28,9 @@ typedef enum bool
  * @next: Pointer to the next element of the list
  */
 typedef struct listint_s
-{
-    const int n;
-    struct listint_s *prev;
-    struct listint_s *next;
+{	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
 } listint_t;
 
 /*Helper for swapping*/
