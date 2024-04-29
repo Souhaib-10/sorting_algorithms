a8432a59791c01a6753961cad4c0bfdbb56f22f0
souhaib10 <souhaibb1998@gmail.com>
2024-04-29 06:55:19 -0700

    create bubble sort

--- sorting_algorithms/sort.h
+++ sorting_algorithms/sort.h
@@ -8,15 +8,15 @@
 #define UP 0
 #define DOWN 1
 
-/*
- * enum bool - Enumeration of boolean values
- * false: Represents false with integer value 0
- * true: Represents true with integer value 1
+/**
+ * enum bool - Enumeration of Boolean values.
+ * @false: Equals 0.
+ * @true: Equals 1.
  */
 typedef enum bool
 {
-	false = 0,  /**< Represents false with integer value 0 */
-	true       /**< Represents true with integer value 1 */
+	false = 0,
+	true
 } bool;
 
 
